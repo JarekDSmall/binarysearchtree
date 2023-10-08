574bfc3cfd53ca0410ea60e0980622379449ae9d
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-08 11:08:21 -0400

    Update binary-search-tree.js to correct file
    on upload accidentally uploaded wrong file so uploaded the js with the proper class and functions

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,280 +1,233 @@
-const BinarySearchTree = require("./binary-search-tree");
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
 
-describe("insert", function() {
-  it("inserts a node at the correct position", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12);
-    expect(binarySearchTree.root.val).toEqual(15);
-    expect(binarySearchTree.root.right.val).toEqual(20);
-    expect(binarySearchTree.root.left.right.val).toEqual(12);
-  });
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
 
-  it("inserts a node at the root if there is nothing there", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree.insert(15);
-    expect(binarySearchTree.root.val).toEqual(15);
-    expect(binarySearchTree.root.left).toBe(null);
-    expect(binarySearchTree.root.right).toBe(null);
-  });
-});
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined; // No duplicates
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 
-describe("insertRecursively", function() {
-  it("inserts a node at the correct position", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insertRecursively(15)
-      .insertRecursively(20)
-      .insertRecursively(10)
-      .insertRecursively(12);
-    expect(binarySearchTree.root.val).toEqual(15);
-    expect(binarySearchTree.root.right.val).toEqual(20);
-    expect(binarySearchTree.root.left.right.val).toEqual(12);
-  });
+  insertRecursively(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    const insertHelper = (node) => {
+      if (val === node.val) return;
+      if (val < node.val) {
+        if (!node.left) {
+          node.left = newNode;
+          return;
+        }
+        insertHelper(node.left);
+      } else {
+        if (!node.right) {
+          node.right = newNode;
+          return;
+        }
+        insertHelper(node.right);
+      }
+    }
+    insertHelper(this.root);
+    return this;
+  }
 
-  it("inserts a node at the root if there is nothing there", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree.insertRecursively(15);
-    expect(binarySearchTree.root.val).toEqual(15);
-    expect(binarySearchTree.root.left).toBe(null);
-    expect(binarySearchTree.root.right).toBe(null);
-  });
-});
+  find(val) {
+    if (!this.root) return undefined;
+    let current = this.root;
+    while (current) {
+      if (val === current.val) return current;
+      if (val < current.val) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return undefined;
+  }
 
-describe("find", function() {
-  it("finds a node correctly", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12);
-    var foundNode = binarySearchTree.find(20);
-    expect(foundNode.val).toBe(20);
-    expect(foundNode.left).toBe(null);
-    expect(foundNode.right).toBe(null);
-  });
+  findRecursively(val) {
+    if (!this.root) return undefined;
+    const findHelper = (node) => {
+      if (!node) return undefined;
+      if (val === node.val) return node;
+      if (val < node.val) {
+        return findHelper(node.left);
+      } else {
+        return findHelper(node.right);
+      }
+    }
+    return findHelper(this.root);
+  }
 
-  it("returns undefined if a node is not found", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12);
-    var foundNode = binarySearchTree.find(120);
-    expect(foundNode).toBe(undefined);
-  });
-});
+  dfsPreOrder() {
+    const result = [];
+    const traverse = (node) => {
+      result.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return result;
+  }
 
-describe("findRecursively", function() {
-  it("finds a node correctly", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12);
-    var foundNode = binarySearchTree.findRecursively(20);
-    expect(foundNode.val).toBe(20);
-    expect(foundNode.left).toBe(null);
-    expect(foundNode.right).toBe(null);
-  });
+  dfsInOrder() {
+    const result = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      result.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return result;
+  }
 
-  it("returns undefined if a node is not found", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12);
-    var foundNode = binarySearchTree.findRecursively(120);
-    expect(foundNode).toBe(undefined);
-  });
-});
+  dfsPostOrder() {
+    const result = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      result.push(node.val);
+    }
+    traverse(this.root);
+    return result;
+  }
 
-describe("dfsPreOrder", function() {
-  it("returns an array of values found with DFS Pre Order", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50);
-    expect(binarySearchTree.dfsPreOrder()).toEqual([15, 10, 1, 5, 12, 20, 50]);
-  });
-});
+  bfs() {
+    const result = [];
+    const queue = [this.root];
+    while (queue.length) {
+      const currentNode = queue.shift();
+      result.push(currentNode.val);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return result;
+  }
 
-describe("dfsInOrder", function() {
-  it("returns an array of values found with DFS In Order", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50);
-    expect(binarySearchTree.dfsInOrder()).toEqual([1, 5, 10, 12, 15, 20, 50]);
-  });
-});
+  remove(val) {
+    const findMin = (node) => {
+      while (node.left) {
+        node = node.left;
+      }
+      return node;
+    };
 
-describe("dfsPostOrder", function() {
-  it("returns an array of values found with DFS Post Order", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50);
-    expect(binarySearchTree.dfsPostOrder()).toEqual([5, 1, 12, 10, 50, 20, 15]);
-  });
-});
+    const removeNode = (node, key) => {
+      if (!node) return null;
+      if (key < node.val) {
+        node.left = removeNode(node.left, key);
+        return node;
+      } else if (key > node.val) {
+        node.right = removeNode(node.right, key);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          node = null;
+          return node;
+        }
+        if (!node.left) {
+          node = node.right;
+          return node;
+        } else if (!node.right) {
+          node = node.left;
+          return node;
+        }
+        const temp = findMin(node.right);
+        node.val = temp.val;
+        node.right = removeNode(node.right, temp.val);
+        return node;
+      }
+    };
 
-describe("BFS", function() {
-  it("should return the correct output", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50);
-    expect(binarySearchTree.bfs()).toEqual([15, 10, 20, 1, 12, 50, 5]);
-  });
-});
+    this.root = removeNode(this.root, val);
+  }
 
-describe("remove", function() {
-  it("remove should correctly remove a node with no children", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50);
-    binarySearchTree.remove(50);
-    expect(binarySearchTree.root.right.val).toBe(20);
-    expect(binarySearchTree.root.right.right).toBe(null);
+  isBalanced() {
+    const checkBalance = (node) => {
+      if (!node) return { isBalanced: true, depth: -1 };
 
-    binarySearchTree.remove(5);
-    expect(binarySearchTree.root.left.left.val).toBe(1);
-    expect(binarySearchTree.root.left.left.right).toBe(null);
-  });
+      const left = checkBalance(node.left);
+      const right = checkBalance(node.right);
 
-  it("remove should correctly remove a node with one child", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50);
+      const isBalanced = left.isBalanced && right.isBalanced && Math.abs(left.depth - right.depth) <= 1;
+      const depth = Math.max(left.depth, right.depth) + 1;
 
-    binarySearchTree.remove(1);
-    expect(binarySearchTree.root.left.left.val).toBe(5);
-    expect(binarySearchTree.root.left.left.left).toBe(null);
-    expect(binarySearchTree.root.left.left.right).toBe(null);
+      return { isBalanced, depth };
+    };
 
-    binarySearchTree.remove(20);
-    expect(binarySearchTree.root.right.val).toBe(50);
-    expect(binarySearchTree.root.right.right).toBe(null);
-    expect(binarySearchTree.root.right.left).toBe(null);
-  });
+    return checkBalance(this.root).isBalanced;
+  }
 
-  it("remove should correctly remove a node with two children", function() {
-    let binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(15)
-      .insert(20)
-      .insert(10)
-      .insert(12)
-      .insert(1)
-      .insert(5)
-      .insert(50)
-      .insert(60)
-      .insert(30)
-      .insert(25)
-      .insert(23)
-      .insert(24)
-      .insert(70);
+  findSecondHighest() {
+    if (!this.root || (!this.root.left && !this.root.right)) return undefined;
 
-    binarySearchTree.remove(10);
-    expect(binarySearchTree.root.left.val).toBe(12);
-    expect(binarySearchTree.root.left.left.val).toBe(1);
-    expect(binarySearchTree.root.left.left.right.val).toBe(5);
+    let current = this.root;
+    let prev = null;
 
-    binarySearchTree.remove(50);
-    expect(binarySearchTree.root.right.val).toBe(20);
-    expect(binarySearchTree.root.right.right.val).toBe(60);
-    expect(binarySearchTree.root.right.right.left.val).toBe(30);
-  });
+    while (current.right) {
+      prev = current;
+      current = current.right;
+    }
 
-  it("should remove a node with two children and handle the children of the removed node", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree
-      .insert(22)
-      .insert(49)
-      .insert(85)
-      .insert(66)
-      .insert(95)
-      .insert(90)
-      .insert(100)
-      .insert(88)
-      .insert(93)
-      .insert(89);
+    if (current.left) {
+      current = current.left;
+      while (current.right) {
+        current = current.right;
+      }
+      return current.val;
+    }
 
-    binarySearchTree.remove(85);
-    expect(binarySearchTree.root.right.right.val).toBe(88); // 88
-    expect(binarySearchTree.root.right.right.right.left.left.val).toBe(89); // 89
-  });
-});
+    return prev.val;
+  }
 
-describe("isBalanced", function() {
-  it("checks if it is balanced", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree.insert(15);
-    binarySearchTree.insert(20);
-    binarySearchTree.insert(10);
-    binarySearchTree.insert(12);
-    expect(binarySearchTree.isBalanced()).toEqual(true);
+  dfsInOrderIteratively() {
+    const result = [];
+    const stack = [];
+    let current = this.root;
 
-    var binarySearchTree2 = new BinarySearchTree();
-    binarySearchTree2.insert(5);
-    expect(binarySearchTree2.isBalanced()).toEqual(true);
-    binarySearchTree2.insert(6);
-    expect(binarySearchTree2.isBalanced()).toEqual(true);
-    binarySearchTree2.insert(7);
-    expect(binarySearchTree2.isBalanced()).toEqual(false);
-  });
-});
+    while (current || stack.length) {
+      while (current) {
+        stack.push(current);
+        current = current.left;
+      }
+      current = stack.pop();
+      result.push(current.val);
+      current = current.right;
+    }
 
-describe("findSecondHighest", function() {
-  it("finds the 2nd largest", function() {
-    var binarySearchTree = new BinarySearchTree();
-    binarySearchTree.insert(15);
-    binarySearchTree.insert(20);
-    binarySearchTree.insert(10);
-    binarySearchTree.insert(12);
-    expect(binarySearchTree.findSecondHighest()).toEqual(15);
+    return result;
+  }
+}
 
-    var binarySearchTree2 = new BinarySearchTree();
-    expect(binarySearchTree2.findSecondHighest()).toEqual(undefined);
-  });
-});
+module.exports = BinarySearchTree;
